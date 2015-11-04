7ef35e34b2c99b3aa1f22e140f2c21bcdc455a29
Matt McHugh <matt.mchugh@excella.com>
2015-11-04 16:13:22 -0500

    Adding EFPaymentDAO

---
+++ Excella.Vending.DAL/EFPaymentDAO.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace Excella.Vending.DAL
+{
+    public class EFPaymentDAO : IPaymentDAO
+    {
+        private VendingMachineContext context = new VendingMachineContext();
+
+        public int Retrieve()
+        {
+            var payment = context.Payments.Where(p => p.ID == 1).FirstOrDefault();
+
+            if (payment != null)
+            {
+                return payment.Value;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void Save(int amount)
+        {
+            var payment = context.Payments.Where(p => p.ID == 1).FirstOrDefault();
+
+            if (payment != null)
+            {
+                payment.Value += amount;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Excella.Vending.DAL/Models/Payment.cs
@@ -0,0 +1,8 @@
+﻿namespace Excella.Vending.DAL.Models
+{
+    public class Payment
+    {
+        public int ID { get; set; }
+        public int Value { get; set; }
+    }
+}
