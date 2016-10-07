6f1557b83714b07ef00844519eb8fc3b801fbcfd
Sean Killeen <SeanKilleen@gmail.com>
2016-10-06 22:58:04 -0400

    Baseline including Selenium based tests

---
+++ Tests.Acceptance.Web.Excella.Vending.Machine/BuyProductSteps.cs
@@ -0,0 +1,102 @@
+﻿using System.Data.SqlClient;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Tests.Acceptance.Web.Excella.Vending.Machine
+{
+    [Binding]
+    public class BuyProductSteps
+    {
+
+        private HomePage _homePage;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            _homePage = new HomePage();
+            _homePage.Go();
+        }
+
+        [AfterScenario]
+        public void Teardown()
+        {
+        }
+
+        [When(@"I insert a quarter")]
+        public void WhenIInsertAQuarter()
+        {
+            InsertQuarter();
+        }
+
+        [Then(@"The balance should be 25 cents")]
+        public void TheBalanceShouldBe25Cents()
+        {
+            var balance = _homePage.Balance();
+
+            Assert.That(balance, Does.Contain("25"));
+            Assert.That(balance, Does.Contain("cents"));
+        }
+        private void InsertQuarter()
+        {
+            _homePage.InsertCoinButton().Click();
+        }
+
+        [Given(@"I have inserted a quarter")]
+        public void GivenIHaveInsertedAQuarter()
+        {
+            InsertQuarter();
+        }
+
+        [When(@"I purchase a product")]
+        public void WhenIPurchaseAProduct()
+        {
+            //try
+            //{
+            //    product = vendingMachine.BuyProduct();
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Product purchase failed: {0}", e.Message);
+            //}
+        }
+
+        [Then(@"I should receive the product")]
+        public void ThenIShouldReceiveTheProduct()
+        {
+            //Assert.IsNotNull(product);
+        }
+
+        [Given(@"I have not inserted a quarter")]
+        public void GivenIHaveNotInsertedAQuarter()
+        {
+            // Not calling insert coin
+        }
+
+        [Then(@"I should not receive a product")]
+        public void ThenIShouldNotReceiveAProduct()
+        {
+            //Assert.IsNull(product);
+        }
+
+
+        private SqlConnection GetConnection()
+        {
+            var connectionString = "Server=.;Database=VendingMachine;Trusted_Connection=True;";
+
+            return new SqlConnection(connectionString);
+        }
+
+        private void ResetDBBalance()
+        {
+            var connection = GetConnection();
+
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand("UPDATE Payment SET Value = 0 WHERE ID = 1;", connection);
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Tests.Acceptance.Web.Excella.Vending.Machine/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests.Acceptance.Web.Excella.Vending.Machine
+{
+    public class HomePage
+    {
+        private readonly ChromeDriver _browser;
+
+
+        public HomePage()
+        {
+            _browser = new ChromeDriver();
+        }
+        const string HOME_PAGE_URL = "http://localhost:8484/";
+        public void Go()
+        {
+            _browser.Navigate().GoToUrl(HOME_PAGE_URL);
+        }
+
+        public IWebElement InsertCoinButton()
+        {
+            IWebElement button = _browser.FindElement(By.Id("insertCoin"));
+            return button;
+        }
+
+        public string Balance()
+        {
+            IWebElement balanceDiv = _browser.FindElement(By.Id("totalAmount"));
+            return balanceDiv.Text;
+        }
+    }
+}
