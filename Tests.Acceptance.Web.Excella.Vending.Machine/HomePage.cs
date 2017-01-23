b459559e19ff20575751e8058874e0efcf3bbe06
Sean Killeen <SeanKilleen@gmail.com>
2017-01-22 19:39:55 -0500

    Remove references

--- Tests.Acceptance.Web.Excella.Vending.Machine/HomePage.cs
+++ Tests.Acceptance.Web.Excella.Vending.Machine/HomePage.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework.Constraints;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.IE;
 
 namespace Tests.Acceptance.Web.Excella.Vending.Machine
 {
