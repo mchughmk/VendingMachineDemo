a81fdc9b311c3ba119863990b9a7eaf6bbd2b938
Sean Killeen <SeanKilleen@gmail.com>
2017-01-22 19:45:52 -0500

    Add underscore to member variable

--- Excella.Vending.DAL/FakePaymentDAO.cs
+++ Excella.Vending.DAL/FakePaymentDAO.cs
@@ -2,27 +2,27 @@
 {
     public class FakePaymentDAO : IPaymentDAO
     {
-        private int balance;
+        private int _balance;
 
         public void SavePayment(int amount)
         {
-            balance += amount;
+            _balance += amount;
         }
 
         public void SavePurchase()
         {
             const int PURCHASE_COST = 50;
-            balance -= PURCHASE_COST;
+            _balance -= PURCHASE_COST;
         }
 
         public void ClearPayments()
         {
-            balance = 0;
+            _balance = 0;
         }
 
         public int Retrieve()
         {
-            return balance;
+            return _balance;
         }
     }
 }
