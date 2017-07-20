bd402da5e78e799d823aca53fa3e234ef011b1f3
Sean Killeen <SeanKilleen@gmail.com>
2017-07-20 15:46:31 -0400

    Move selenium functionality into test class
    Removes page object model (per Fadi, it's tough to express this concept
    in training usually).

--- Tests.Acceptance.Web.Excella.Vending.Machine/BuyProductSteps.cs
+++ Tests.Acceptance.Web.Excella.Vending.Machine/BuyProductSteps.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using TechTalk.SpecFlow;
 // ReSharper disable UnusedMember.Global -- test methods are said to be unused which isn't correct. -SK
 
@@ -8,7 +10,8 @@ namespace Tests.Acceptance.Web.Excella.Vending.Machine
     [Binding]
     public class BuyProductSteps
     {
-        private HomePage _homePage;
+        private IWebDriver _browser;
+        const string HOME_PAGE_URL = "http://localhost:8484/";
 
         [BeforeFeature]
         public static void BeforeFeature()
@@ -30,39 +33,38 @@ namespace Tests.Acceptance.Web.Excella.Vending.Machine
             {
                 throw new Exception("IIS Express must be running for this test to work");
             }
-            _homePage = new HomePage();
-            _homePage.Go();
+            _browser = new ChromeDriver();
+
+            GoToHomePage();
         }
 
         [AfterScenario]
         public void Teardown()
         {
-            _homePage?.ReleaseChangeButton().Click();
-            _homePage?.Close();
+            ClickReleaseChangeButton();
+
+            _browser.Close();
+            _browser.Dispose();
         }
 
         [When(@"I insert a Quarter")]
         public void WhenIInsertAQuarter()
         {
-            InsertQuarter();
+            ClickInsertCoinButton();
         }
 
         [Then(@"The balance should be (.*) cents")]
         public void TheBalanceShouldBe(int cents)
         {
-            var balance = _homePage.Balance();
+            var balance = GetBalance();
 
             Assert.That(balance, Is.EqualTo(cents));
-        }
-        private void InsertQuarter()
-        {
-            _homePage.InsertCoinButton().Click();
         }
 
         [Given(@"I have inserted a quarter")]
         public void GivenIHaveInsertedAQuarter()
         {
-            InsertQuarter();
+            ClickInsertCoinButton();
         }
 
         [When("I do not purchase a product")]
@@ -80,13 +82,13 @@ namespace Tests.Acceptance.Web.Excella.Vending.Machine
         [When(@"I release the change")]
         public void WhenIReleaseTheChange()
         {
-            _homePage.ReleaseChangeButton().Click();
+            ClickReleaseChangeButton();
         }
 
         [Then(@"I should receive (.*) cents in change")]
         public void ThenIShouldReceiveXCentsInChange(int cents)
         {
-            var releasedChange = _homePage.ReleasedChange();
+            var releasedChange = GetReleasedChange();
 
             Assert.That(releasedChange, Is.EqualTo(cents));
         }
@@ -108,6 +110,58 @@ namespace Tests.Acceptance.Web.Excella.Vending.Machine
         public void ThenIShouldNotReceiveAProduct()
         {
             //Assert.IsNull(product);
+        }
+
+        private void GoToHomePage()
+        {
+            _browser.Navigate().GoToUrl(HOME_PAGE_URL);
+        }
+
+        private void ClickReleaseChangeButton()
+        {
+            IWebElement button = _browser.FindElement(By.Id("releaseChange"));
+
+            button.Click();
+        }
+
+        private int GetReleasedChange()
+        {
+            try
+            {
+                var element = _browser.FindElement(By.Id("releasedChangeAmount")).Text;
+
+                int changeAmt;
+                if (int.TryParse(element, out changeAmt))
+                {
+                    return changeAmt;
+                }
+
+                return 0;
+            }
+            catch (StaleElementReferenceException)
+            {
+                return GetReleasedChange();
+            }
+
+        }
+
+        private void ClickInsertCoinButton()
+        {
+            IWebElement button = _browser.FindElement(By.Id("insertCoin"));
+            button.Click();
+        }
+
+        private int GetBalance()
+        {
+            try
+            {
+                return int.Parse(_browser.FindElement(By.Id("balanceAmount")).Text);
+            }
+            catch (StaleElementReferenceException)
+            {
+                return GetBalance();
+            }
+
         }
     }
 }
