0075ab809490f595ef96376769610b5900c5103e
Sean Killeen <SeanKilleen@gmail.com>
2017-07-21 22:15:08 -0400

    Add ViewModel

---
+++ Excella.Vending.Web.UI/Models/VendingMachineViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Excella.Vending.Web.UI.Models
+{
+    public class VendingMachineViewModel
+    {
+        public double Balance { get; set; }
+        public double ReleasedChange { get; set; }
+    }
+}
