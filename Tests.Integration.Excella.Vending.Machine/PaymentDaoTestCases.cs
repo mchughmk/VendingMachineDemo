ac0b99a2a6372e81e5060a3efa5d54973ddcf80d
Sean Killeen <SeanKilleen@gmail.com>
2017-07-23 17:50:04 -0400

    extract payment dao test cases

---
+++ Tests.Integration.Excella.Vending.Machine/PaymentDaoTestCases.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Excella.Vending.DAL;
+
+namespace Tests.Integration.Excella.Vending.Machine
+{
+    public class PaymentDaoTestCases
+    {
+        public static IEnumerable<object> TestCases
+        {
+            get
+            {
+                yield return new EFPaymentDAO();
+                yield return new ADOPaymentDAO();
+            }
+        }
+    }
+}
