bff7c78e548064079573e444149dd8f3168c3e7c
Sean Killeen <SeanKilleen@gmail.com>
2017-07-23 18:14:58 -0400

    clean up linq

--- Excella.Vending.DAL/EFPaymentDAO.cs
+++ Excella.Vending.DAL/EFPaymentDAO.cs
@@ -8,7 +8,7 @@ namespace Excella.Vending.DAL
 
         public int Retrieve()
         {
-            var payment = _context.Payments.Where(p => p.Id == 1).FirstOrDefault();
+            var payment = _context.Payments.FirstOrDefault(p => p.Id == 1);
 
             if (payment != null)
             {
@@ -20,7 +20,7 @@ namespace Excella.Vending.DAL
 
         public void SavePayment(int amount)
         {
-            var payment = _context.Payments.Where(p => p.Id == 1).FirstOrDefault();
+            var payment = _context.Payments.FirstOrDefault(p => p.Id == 1);
 
             if (payment != null)
             {
@@ -32,7 +32,7 @@ namespace Excella.Vending.DAL
         public void SavePurchase()
         {
             const int PURCHASE_COST = 50;
-            var payment = _context.Payments.Where(p => p.Id == 1).FirstOrDefault();
+            var payment = _context.Payments.FirstOrDefault(p => p.Id == 1);
 
             if (payment != null)
             {
@@ -43,7 +43,7 @@ namespace Excella.Vending.DAL
 
         public void ClearPayments()
         {
-            var payment = _context.Payments.Where(p => p.Id == 1).FirstOrDefault();
+            var payment = _context.Payments.FirstOrDefault(p => p.Id == 1);
 
             if (payment != null)
             {
