40bdaddba922f9f19478d61ea23d9fdee3e1533f
Sean Killeen <SeanKilleen@gmail.com>
2017-07-23 18:18:20 -0400

    unnecessary using statements

--- Excella.Vending.Web.UI/Models/VendingMachineViewModel.cs
+++ Excella.Vending.Web.UI/Models/VendingMachineViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Excella.Vending.Web.UI.Models
+﻿namespace Excella.Vending.Web.UI.Models
 {
     public class VendingMachineViewModel
     {
